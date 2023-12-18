207f1c9f94f0bd97f063c530a0c9de25f9a7bbe4
fakrouna <154240263+fakrouna@users.noreply.github.com>
2023-12-18 15:18:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var randomNumber1= Math.floor((Math.random() *6 +1))
-const image1 = document.getElementsByClassName("img1")
-image1.setAttribute("src", "images/dice"+randomNumber1+".png")
+var randomNumber1= Math.floor((Math.random() *6 +1));
+const image1 = document.getElementsByClassName("img1");
+image1.setAttribute("src", "images/dice"+randomNumber1+".png");
 
-var randomNumber2= Math.floor((Math.random() *6 +1))
-const image2 = document.getElementsByClassName("img2")
-image2.setAttribute("src", "images/dice"+randomNumber2+".png")
+var randomNumber2= Math.floor((Math.random() *6 +1));
+const image2 = document.getElementsByClassName("img2");
+image2.setAttribute("src", "images/dice"+randomNumber2+".png");
 
 
 
@@ -12,14 +12,14 @@ image2.setAttribute("src", "images/dice"+randomNumber2+".png")
 
 function findTitle(n1,n2){
 
-    if ( n1 === null ){return "Refresh"}
+    if ( n1 === null ){return "Refresh";}
 
-    if(n1>n2){ return "Player 1 Wins"}
-    else if(n2>n1){return "player 2 Wins"}
-    else if(n1==n2) {return "Draw !"}
+    if(n1>n2){ return "Player 1 Wins";}
+    else if(n2>n1){return "player 2 Wins";}
+    else if(n1==n2) {return "Draw !";}
 }
 
-var title = document.getElementsByTagName("h1")[0]
-title.innerHTML = findTitle(randomNumber1,randomNumber2)
+var title = document.getElementsByTagName("h1")[0];
+title.innerHTML = findTitle(randomNumber1,randomNumber2);
     
 

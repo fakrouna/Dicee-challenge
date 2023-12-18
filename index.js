55909660994f31cef313ee63c2acb8618d266f67
fakrouna <154240263+fakrouna@users.noreply.github.com>
2023-12-18 15:24:13 +0100

    add .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var randomNumber1= Math.floor((Math.random() *6 +1));
-document.getElementsByClassName("img1")[0]setAttribute("src", "images/dice"+randomNumber1+".png");
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice"+randomNumber1+".png");
 
 
 var randomNumber2= Math.floor((Math.random() *6 +1));
